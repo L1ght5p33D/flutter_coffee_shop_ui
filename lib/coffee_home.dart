2916d0e7d6618080f9bc11b1a37f049b26cc7159
ls <ryanadrig@gmail.com>
2023-02-03 20:16:23 -0800

    home list scroll

--- lib/coffee_home.dart
+++ lib/coffee_home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-
+List coffee_types_list = ["Cappuccino","Espresso", "Latte", "Flat White"];
 
 class CoffeeShopHome extends StatefulWidget {
   const CoffeeShopHome({super.key});
@@ -12,6 +12,7 @@ class CoffeeShopHome extends StatefulWidget {
 
 class _CoffeeShopHomeState extends State<CoffeeShopHome> {
 
+int coffee_type_chosen_idx=0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +20,15 @@ class _CoffeeShopHomeState extends State<CoffeeShopHome> {
     var ss = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        child:Container(
+        child:Padding(
+            padding: EdgeInsets.all(ss.width*.03),
+            child:Container(
             height: ss.height,
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+            child: ListView(
           children: <Widget>[
+            Container(
+            height: ss.width * .13,
+            child:
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -40,10 +45,79 @@ class _CoffeeShopHomeState extends State<CoffeeShopHome> {
                     color: Colors.grey[500],
                   ),
                 ),
-            ],)
+              ClipRRect(borderRadius: BorderRadius.circular(ss.width * .06),
+              child:Image.asset("assets/images/profile_avatar.png",
+                  width: ss.width*.12,
+                  height: ss.width * .12,
+                  fit:BoxFit.contain)
+              )
+
+            ],)),
+
+            Container(
+                height: ss.width * .18,
+                child:
+            Text("Find your favorite coffee drinks",
+            style:TextStyle(fontSize: ss.width * .15))),
+
+            Container(
+              padding: EdgeInsets.symmetric(vertical: ss.width*.03),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(ss.width * .05),
+              ),
+                width: ss.width * .88,
+                child:Row(children:[
+                  Icon(Icons.search,
+                  size: ss.width * .045,),
+                  TextField(decoration: InputDecoration(
+                      border: InputBorder.none,
+                      // labelText: 'Enter Name',
+                      hintText: 'Find Coffee'
+                  ),  )
+                ])
+            ),
+
+            ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, coffee_type_idx){
+                  Color itemtextcolor = Colors.grey[400]!;
+                  bool item_is_chosen = false;
+                  if (coffee_type_chosen_idx == coffee_type_idx){
+                    itemtextcolor = Colors.deepOrange;
+                    item_is_chosen = true;
+                  }
+                  return
+                  Container(
+                      width: ss.width * .25,
+                      height: ss.width * .15,
+                      child:Column(children:[
+                    InkWell(
+                        onTap:(){
+                          setState(() {
+                            coffee_type_chosen_idx = coffee_type_idx;
+                          });
+                        },
+                        child:
+                    Container(
+                      width: ss.width * .25,
+                      child:Center(child:
+                      Text(coffee_types_list[coffee_type_idx],
+                            style: TextStyle(color: itemtextcolor),
+                      )))
+                    ),
+                      item_is_chosen ?
+                          Text(".",
+                          style: TextStyle(fontSize: ss.width*.5,
+                                            color: itemtextcolor),):
+                      Container()
+
+                      ]));
+            })
+
+
 
           ],
-        )),
+        ))),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:coffee_shop_ui/coffee_home.dart';
 class CustomError extends StatelessWidget {
   CustomError({Key? key, this.errorDetails}) : super(key: key);
   FlutterErrorDetails? errorDetails;
+
   @override
   Widget build(BuildContext context) {
     return ListView(children: [
@@ -14,13 +15,14 @@ class CustomError extends StatelessWidget {
                   style: TextStyle(color: Colors.black))))
     ]);
   }
+}
 
 void main() {
-  runApp(const CoffeeShopApp());
+  runApp(CoffeeShopApp());
 }
 
 class CoffeeShopApp extends StatelessWidget {
-  const CoffeeShopApp({super.key});
+  CoffeeShopApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +30,9 @@ class CoffeeShopApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
+        brightness: Brightness.dark
+  ),
+
         builder: (BuildContext context, Widget? widget) {
           ErrorWidget.builder = (FlutterErrorDetails errorDetails) {
             return CustomError(errorDetails: errorDetails);
